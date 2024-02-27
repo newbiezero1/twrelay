c285a0d55e1c5359fbc40be9f0542fcfed06696e
andsnd <kentozavr01@gmail.com>
2024-02-27 11:18:30 +0200

    call for new tweet

--- call.py
+++ call.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 import config
 
@@ -13,3 +14,24 @@ def call_to_worker():
     response = requests.request("POST", url, headers=headers, data=payload)
 
     print(response.json())
+
+
+def get_last_msg_date() -> int:
+    response = requests.get(f'https://api.telegram.org/bot{config.tg_api_key}/getUpdates?offset=-1')
+    data = response.json()["result"]
+    if not data:
+        return 0
+    if 'message' in data[0]:
+        return data[0]['message']['date']
+    return 0
+
+
+def get_last_tweet_date() -> int:
+    with open(config.last_tweet_file) as f:
+        data = f.read().strip()
+    return json.loads(data)['time']
+
+last_update = get_last_msg_date()
+last_tweet_date = get_last_tweet_date()
+if last_tweet_date > last_update:
+    call_to_worker()
